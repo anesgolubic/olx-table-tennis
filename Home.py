d3e66c6adbc42edf1ad419f88f70ed6b5214facc
anesgolubic <anes.golubic@gmail.com>
2023-10-22 19:33:30 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -38,3 +38,6 @@ for index, row in last_matches.iterrows():
     with col4:
         st.markdown('<h5>'+str(row['Protivnik_2'])+'</h5>', unsafe_allow_html=True)
     st.divider()
+
+for index, row in last_matches.iterrows():
+    st.markdown('<h5>'+str(row['Protivnik_1'])+'</h5><h4>'+str(row['Rezultat_1'])+' : '+str(row['Rezultat_2'])+'</h4><h5>'+str(row['Protivnik_2'])+'</h5>', unsafe_allow_html=True)
