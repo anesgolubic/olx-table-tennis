471fc7f1a9609b3853c6043b60d0985718ac7a52
anesgolubic <anes.golubic@gmail.com>
2023-10-22 19:18:24 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -27,16 +27,3 @@ st.write(last_matches)
 
 #def rezultat_matcha(id):
 
-for index,row in last_matches.iterrows():
-    html = '''<div class="utakmica"><div class="datum">
-    <h5>'''+str(row['Datum_meča'])+'''</h5></div>
-    <div class="protivnik">
-    <h4>'''+row['Protivnik_1']+'''</h4></div>
-    <div class="protivnik">
-    <h4>'''+row['Protivnik_2']+'''</h4></div>
-    <div class="rezultat">
-    <h4>'''+row['Rezultat_1']+'''</h4></div>
-    <div class="rezultat">
-    <h4>'''+row['Rezultat_2']+'''</h4></div></div>
-    '''
-    st.markdown(html, unsafe_allow_html=True)
