8c6d8442a66c53c99b107210a5744aeea4fb0c4d
anesgolubic <anes.golubic@gmail.com>
2023-10-22 18:44:56 +0200

    Initial commit

---
+++ Home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+st.set_page_config(
+    page_title="OLX.ba Table Tennis Liga",
+    #page_icon="🧊",
+    layout="wide",
+)
+
+import pandas as pd
+
+# LINK TO THE CSS FILE
+with open('style.css')as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
+
+"""
+# OLX.ba Table Tennis Liga
+"""
+
+matches = pd.read_excel('table_tennis.xlsx', sheet_name='Matchevi')
+
+st.write(matches)
